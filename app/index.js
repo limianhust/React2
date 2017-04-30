e900ce30da14b53608d187ba7f7e22be396b3068
李冕 <limian@limiandeMacBook-Air.local>
2017-04-30 10:07:59 +0800

    first commit

---
+++ app/index.js
@@ -0,0 +1,17 @@
+import _ from 'lodash';
+import $ from 'jquery'
+import foo from './foo'
+function component () {
+  var $element = $('<div></div>');
+
+  /* lodash is required for the next line to work */
+  $element.html(_.join(['Hello','webpack'], ' '));
+
+  return $element;
+}
+
+$('body').append(component());
+console.log(foo)
+console.log(foo())
+console.log('npm run bulid')
+
