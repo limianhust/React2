b6cca0aca1795bf0886477b95ef89a364751c4b7
李冕 <limian@limiandeMacBook-Air.local>
2017-05-03 21:55:48 +0800

    secend

--- app/index.js
+++ app/index.js
@@ -5,7 +5,7 @@ function component () {
   var $element = $('<div></div>');
 
   /* lodash is required for the next line to work */
-  $element.html(_.join(['Hello','webpack'], ' '));
+  $element.html(_.join(['Hi','webpack'], ' '));
 
   return $element;
 }
