66f054200dce22b6fb9a8db05b31715b42a68e37
李冕 <limian@limiandeMacBook-Air.local>
2017-05-14 09:21:47 +0800

    mission 3

--- src/Welcome.js
+++ src/Welcome.js
@@ -1,7 +1,33 @@
 import React from 'react';
 class Welcome extends React.Component {
+	constructor(props){
+		super(props)
+		this.state = {
+			date: new Date()
+		}
+		setInterval(()=>{
+				this.setState({
+					date: new Date()
+				})
+		})
+		console.log('我已经在constructor里将 props 和 state 初始化好了')
+	}
+	componentWillMount(){
+		console.log('运行到这里的话，说明马上要运行 render 了')
+	}
 	render(){
-		return <h1>Hello,Component</h1>;
-        }
+		//return <h1>Hello,{this.props.name}</h1>;
+		console.log('嗯，这里是render')
+		return (
+			<div>
+				<h1>Hello,{this.props.name}</h1>
+				<h2>{this.state.date.toString()}</h2>
+			</div>
+		)
+    }
+	componentDidMount(){
+		console.log('以及挂载到页面里了')
+	}
 }
+
 export default Welcome
--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+//import App from './App';
 import './index.css';
 import Welcome from './Welcome'
 
+// function Welcome(props) {
+//   const element = (<h1>Hello,{props.name}</h1>);
+  
+// }
+// Welcome({name: 'limian'})
 ReactDOM.render(
-  <Welcome/>, 
+  <Welcome/>,
   document.getElementById('root')
-);
+  );
+
+
+
